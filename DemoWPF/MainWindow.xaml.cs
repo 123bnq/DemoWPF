ef7061c8315f52d2ee83b1786e7d2994de1f1c01
123bnq <33340665+123bnq@users.noreply.github.com>
2018-11-30 00:28:11 +0100

    working with CheckBox

--- DemoWPF/MainWindow.xaml.cs
+++ DemoWPF/MainWindow.xaml.cs
@@ -44,11 +44,62 @@ namespace DemoWPF
 
         private void btn1_Click(object sender, RoutedEventArgs e)
         {
-            List.Items.Add("Hello");
-            List.Items.Add(10);
-            //List.Items.Add(pnlMainPanel.FindResource(aNumber).ToString());
-            //List.Items.Add(this.FindResource(strHelloWorld).ToString());
-            //List.Items.Add(Application.Current.FindResource(strApp));
+            if (List.Items.Count >= 10)
+            {
+                List.Items.Clear();
+            }
+            List.Items.Add(pnlMainPanel.FindResource("aNumber").ToString());
+            List.Items.Add(pnlMainPanel.FindResource("strHelloWorld").ToString());
+            //List.Items.Add(pnlMainPanel.FindResource("strApp").ToString());
+            List.Items.Add(this.FindResource("strApp").ToString());
+            //List.Items.Add(Application.Current.FindResource("strApp"));
+            
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.Uri.AbsoluteUri);
+        }
+
+        private void txtName_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                e.Handled = true;
+                MessageBox.Show($"Name is set", "Notify", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void txtName_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            txtStatus.Clear();
+            TextBox textBox = sender as TextBox;
+            if (textBox.SelectionLength > 0)
+            {
+                txtStatus.Text = new StringBuilder($"Selection starts at character #{textBox.SelectionStart}\nSelection is {textBox.SelectionLength} charater(s) long\n" +
+                    $"Selected Text: '{textBox.SelectedText}'").ToString();
+            }
+        }
+
+        private void cbAll_CheckedChanged(object sender, RoutedEventArgs e)
+        {
+            bool condition = (cbAll.IsChecked == true);
+            cbA.IsChecked = condition;
+            cbB.IsChecked = condition;
+            cbC.IsChecked = condition;
+        }
+
+        private void cbFeature_CheckedChanged(object sender, RoutedEventArgs e)
+        {
+            cbAll.IsChecked = null;
+            if((cbA.IsChecked == true) && (cbB.IsChecked==true) && (cbC.IsChecked == true))
+            {
+                cbAll.IsChecked = true;
+            }
+            if ((cbA.IsChecked == false) && (cbB.IsChecked == false) && (cbC.IsChecked == false))
+            {
+                cbAll.IsChecked = false;
+            }
         }
     }
 }
